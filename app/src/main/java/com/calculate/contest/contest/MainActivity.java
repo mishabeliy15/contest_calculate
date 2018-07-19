30ce45167bee434782a14c7c2a949cf434f8fdf1
misha.beliy15 <misha.beliy15@gmail.com>
2018-07-19 15:21:01 +0300

    bug fixed

--- app/src/main/java/com/calculate/contest/contest/MainActivity.java
+++ app/src/main/java/com/calculate/contest/contest/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -16,26 +15,25 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import static java.lang.Thread.sleep;
-import static java.util.Arrays.sort;
+import static java.util.Collections.sort;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sPref;
     final String contest_score = "181.458", my_url = "";
     EditText ed_score, ed_myurl;
-    TextView prior1_view, prior2_view, prior3_view, prior_err_view, all_zayav_view, before_me_view, view_err,view_last_year,textview_last_p1;
+    TextView prior1_view, prior2_view, prior3_view, prior_err_view, all_zayav_view, before_me_view, view_err, view_last_year,
+            textview_last_p1, last_upd_v,view_budget;
     RadioGroup radio_group;
     CheckBox checkbox_last_year;
     final String[] urls_osvita = {"https://vstup.osvita.ua/r21/92/460953/", "https://vstup.osvita.ua/r21/92/461081/",
             "https://vstup.osvita.ua/r21/227/445717/"},
             urls_abit = {"https://abit-poisk.org.ua/rate2017/direction/46784/?page=", "https://abit-poisk.org.ua/rate2017/direction/46789/?page=",
-            "https://abit-poisk.org.ua/rate2017/direction/44835/?page="};
+                    "https://abit-poisk.org.ua/rate2017/direction/44835/?page="};
     int url_id = 0;
 
     @Override
@@ -49,12 +47,14 @@ public class MainActivity extends AppCompatActivity {
         prior_err_view = findViewById(R.id.textView_prior_err);
         all_zayav_view = findViewById(R.id.textView_all_zayav);
         before_me_view = findViewById(R.id.textView_before);
+        last_upd_v = findViewById(R.id.textView_last_upd);
         ed_myurl = findViewById(R.id.editText_my_url);
         view_err = findViewById(R.id.textView_errors_count);
         radio_group = findViewById(R.id.radioGroup);
         checkbox_last_year = findViewById(R.id.checkBox);
         view_last_year = findViewById(R.id.textView_prev_year);
         textview_last_p1 = findViewById(R.id.textView_prohod);
+        view_budget = findViewById(R.id.textView_budget);
         radio_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -127,82 +127,74 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 Document doc = null;
                 Elements abits = null;
-                String last_upd = null;
-                String budgets = null;
-                int budg = 0;
-                ArrayList<Double> balls_p1=null;
+                ArrayList<Double> balls_p1 = null;
                 try {
                     doc = get_document(url);
-                    abits = doc.select("table.rwd-table > tbody > tr[class*=rstatus]:not(.hdn)");
-                    if (last_upd == null) last_upd = doc.selectFirst("div.last-update").text();
-                    Elements bud = doc.select("div[class=table-of-specs-item panel-mobile]");
-                    if (budgets == null) {
-                        StringBuilder temp = new StringBuilder(bud.text());
-                        temp = temp.delete(0,temp.indexOf("Максимальне держзамовлення:"));
-                        for (int i=0;i<2;i++)temp = temp.delete(0,temp.indexOf(" ")+1);
-                        temp = temp.delete(temp.indexOf(" "),temp.length());
-                        budgets = temp.toString();
-                        budg = Integer.parseInt(budgets);
-                    }
-                    double my_score = Double.parseDouble(ed_score.getText().toString());
-                    int prior1 = 0, prior2 = 0, prior3 = 0, prior_c = 0, counts_before_me = 0;
-                    balls_p1 = new ArrayList();
-                    int i=0;
-                    for (Element abit : abits) {
-                        String temp;
-                        try {
-                            temp = abit.select("td[data-th=П]").first().text();
-                            int prior = !temp.contains("—") ? Integer.parseInt(temp) : 0;
-                            double score = Double.parseDouble(abit.select("td[data-th=Бал]").first().text());
-                            if (my_score < score) {
-                                counts_before_me++;
-                                switch (prior) {
-                                    case 0:
-                                        prior_c++;
-                                        break;
-                                    case 1:
-                                        prior1++;
-                                        break;
-                                    case 2:
-                                        prior2++;
-                                        break;
-                                    case 3:
-                                        prior3++;
-                                        break;
-                                }
-                            }
-                            if(prior==1){
-                                balls_p1.add(score);
-                                i++;
-                            }
-                        } catch (Exception e) {
-                        }
-                    }
-                    final int f_pr1 = prior1, f_pr2 = prior2, f_pr3 = prior3, f_c = prior_c, all_z = abits.size(), f_before_me = counts_before_me;
-                    final String temp_upd = last_upd, f_budgets = budgets;
-                    sort(balls_p1.toArray(), Collections.reverseOrder());
-                    final double prohod = balls_p1.get(budg>balls_p1.size()?balls_p1.size():budg-1);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            all_zayav_view.setText(String.valueOf(all_z));
-                            before_me_view.setText(String.valueOf(f_before_me));
-                            prior1_view.setText(String.valueOf(f_pr1));
-                            prior2_view.setText(String.valueOf(f_pr2));
-                            prior3_view.setText(String.valueOf(f_pr3));
-                            prior_err_view.setText(String.valueOf(f_c));
-                            TextView last_upd_v = findViewById(R.id.textView_last_upd);
-                            last_upd_v.setText(temp_upd);
-                            last_upd_v.setVisibility(View.VISIBLE);
-                            TextView view_budget = findViewById(R.id.textView_budget);
-                            view_budget.setText(f_budgets);
-                            textview_last_p1.setText(String.valueOf(prohod));
-                        }
-                    });
-                } catch (Exception e) {
+                } catch (InterruptedException e) {
+                    //e.printStackTrace();
                 }
+                String last_upd = doc.selectFirst("div.last-update").text();
+                int budg = getBudgets(doc);
+                abits = doc.select("table.rwd-table > tbody > tr[class*=rstatus]:not(.hdn)");
+                double my_score = Double.parseDouble(ed_score.getText().toString());
+                int prior1 = 0, prior2 = 0, prior3 = 0, prior_c = 0, counts_before_me = 0;
+                balls_p1 = new ArrayList();
+                for (Element abit : abits) {
+                    String temp;
+                    temp = abit.select("td[data-th=П]").first().text();
+                    int prior = !temp.contains("—") ? Integer.parseInt(temp) : 0;
+                    double score = Double.parseDouble(abit.select("td[data-th=Бал]").first().text());
+                    if (prior == 1) balls_p1.add(score);
+                    if (my_score < score) {
+                        counts_before_me++;
+                        switch (prior) {
+                            case 0:
+                                prior_c++;
+                                break;
+                            case 1:
+                                prior1++;
+                                break;
+                            case 2:
+                                prior2++;
+                                break;
+                            case 3:
+                                prior3++;
+                                break;
+                        }
+                    }
+                }
+                final int f_pr1 = prior1, f_pr2 = prior2, f_pr3 = prior3, f_c = prior_c, all_z = abits.size(),
+                        f_before_me = counts_before_me, f_budgets = budg;
+                final String temp_upd = last_upd;
+                sort(balls_p1, Collections.<Double>reverseOrder());
+                final double prohod = balls_p1.get(budg <= balls_p1.size() ? (budg - 1):balls_p1.size());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        all_zayav_view.setText(String.valueOf(all_z));
+                        before_me_view.setText(String.valueOf(f_before_me));
+                        prior1_view.setText(String.valueOf(f_pr1));
+                        prior2_view.setText(String.valueOf(f_pr2));
+                        prior3_view.setText(String.valueOf(f_pr3));
+                        prior_err_view.setText(String.valueOf(f_c));
+                        last_upd_v.setText(temp_upd);
+                        last_upd_v.setVisibility(View.VISIBLE);
+                        view_budget.setText(String.valueOf(f_budgets));
+                        textview_last_p1.setText(String.valueOf(prohod));
+                    }
+                });
+
             }
         }).start();
+    }
+
+    protected int getBudgets(Document doc) {
+        Elements bud = doc.select("div[class=table-of-specs-item panel-mobile]");
+        StringBuilder temp = new StringBuilder(bud.text());
+        temp = temp.delete(0, temp.indexOf("Максимальне держзамовлення:"));
+        for (int i = 0; i < 2; i++) temp = temp.delete(0, temp.indexOf(" ") + 1);
+        temp = temp.delete(temp.indexOf(" "), temp.length());
+        return Integer.parseInt(temp.toString());
     }
 
     protected void preview_year(final String url) {
@@ -251,7 +243,7 @@ public class MainActivity extends AppCompatActivity {
         do
             try {
                 doc = Jsoup.connect(url).get();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 errors_pars++;
                 final int temp_err = errors_pars;
                 runOnUiThread(new Runnable() {
